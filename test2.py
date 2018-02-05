be9dd4468d48266c55f83a3af3f118e98093698e
Alex <roya0045@users.noreply.github.com>
2018-02-05 12:43:41 -0500

    added weight importing support and more params

--- test2.py
+++ test2.py
@@ -12,8 +12,9 @@ import keras.layers as kl
 import keras.optimizers as ko
 import functools as ft
 #ARU.layer_interp(nlayers, inpu, method)
-
-K=1#keras trainer trigger
+filelog=1
+importW=0
+K=1
 
 #heads, may add dropout
 a1=['conv','conv','pool']
@@ -29,15 +30,18 @@ a8=['proto','conv','pool','conv']
 b1=['flat','dense']
 b2=['flat','dense','dense']
 b3=['flat','dense','dense','dense']
+b4=['flat','dense','dense','dense','dense']
 
 #################VALUES THAT CAN BE CHANGED################################
 
 #################STRUCTURE
 
 kerdict=a1+b2
+todo=[a1+b1,['conv','proto','pool']+b1]
 #layers to use, use 1 from a and one from b
 
-optim=ko.RMSprop(lr=0.001, rho=0.9, epsilon=None, decay=0.0001)#.optimizers.Nadam(lr=0.005, beta_1=0.9, beta_2=0.999, epsilon=None, schedule_decay=0.004)
+optim=ko.RMSprop(lr=0.081, rho=0.9, epsilon=None, decay=0.0001)
+optim2=k.optimizers.Nadam(lr=0.99, beta_1=0.9, beta_2=0.999, epsilon=None, schedule_decay=0.004)
 #optimizer to use, see keras or tf docs for alternatives
 
 ################TRAINING
@@ -45,15 +49,19 @@ optim=ko.RMSprop(lr=0.001, rho=0.9, epsilon=None, decay=0.0001)#.optimizers.Nada
 batchs=23
 #0#10 #size of the batch
 
-epochs=5
+epochs=2
 # number of training iterations
 
 ################LAYER SETTINGS
 
 layer_sizes=[20,50,100,30,]
+if importW:
+    layer_sizes=[64,64,1000]#[64,64,128,128,1000]
 #number of "cells" per layers
 
 window_size=[6,4]
+if importW:
+    window_size=[3,]
 #window size to use for conv and proto layer
 
 pool_size=2
@@ -71,10 +79,11 @@ fracto=0
 #fraction of the data to use, set to 0 for all data
 
 ################MISC
+frezimp=1#freeze imported weights
 proto=0#changes the parent class (0:_conv,1: base layer)
 seq=0 #use the sequential model builder instead of the API
 outft=0#used to coerce output data type to match NN output in case
-eval=0#perform evaluation step at the end of training
+evalu=0#perform evaluation step at the end of training
 ################# END OF VALUES THAT CAN BE CHANGED################################
 
 
@@ -94,7 +103,8 @@ tout=ft.partial(tout,**arg) if outft==0 else ft.partial(tout,typ=arg)
 tftrain=(np.expand_dims(np.float32(train[0]), 1 if channel_order=="first" else -1),tout(train[1]))
 tftest=(np.expand_dims(np.float32(test[0]), 1 if channel_order=="first" else -1),tout(test[1] ))
 
-def kertrain(*args):
+
+def kertrain(*args,wd=None,frezimp=False):
     kerdict=list()
     for arg in args:
         kerdict.append(arg)
@@ -104,19 +114,21 @@ def kertrain(*args):
             "pool":((),{"pool_size":(pool_size,pool_size),"data_format":"channels_{}".format(channel_order),}),"dense":((layer_sizes,),{"activation":act}),"flat":((),{}),"drop":(0.25,{})}
     kcw=window_size[-1]
     lss=layer_sizes[-1]
+    winds=copy.deepcopy(window_size)
+    lays=copy.deepcopy(layer_sizes)
     if seq:
         model=k.Sequential()
         for ix,I in enumerate(kerdict):#sequential model
             if (len(kwargs[I][0])>0) and not(I=="drop" or I=="flat" or I=="pool"):
                 try:
-                    sss=(layer_sizes.pop(0),)
+                    sss=(lays.pop(0),)
                 except:
                     sss=(lss,)
             else:
                 sss=kwargs[I][0]
             if (I=="conv")or(I=='proto'):
                 try:
-                    wand=window_size.pop(0)
+                    wand=winds.pop(0)
                 except:
                     want=kcw
                 twand=(wand,wand)
@@ -137,14 +149,14 @@ def kertrain(*args):
         for ix,U in enumerate(kerdict):#func api
             if (len(kwargs[U][0])>0) and not(U=="drop" or U=="flat"or U=="pool"):
                 try:
-                    sss=(layer_sizes.pop(0),)
+                    sss=(lays.pop(0),)
                 except:
                     sss=(lss,)
             else:
                 sss=kwargs[U][0]
             if (U=="conv")or(U=='proto'):
                 try:
-                    wand=window_size.pop(0)
+                    wand=winds.pop(0)
                 except:
                     want=kcw
                 twand=(wand,wand)
@@ -162,15 +174,28 @@ def kertrain(*args):
                 else:
                     KERDICT[ix-iv]= klks[U](*sss,**kwargs[U][1])(KERDICT[ix-iv-1])
         model=k.Model(inputs=kinput,output=KERDICT[-1])
+    if importW:
+        for ix,I in enumerate(model.layers[1:]):
+            ol=[o.shape for o in I.weights]
+            if len(ol)>1:
+                for it,t in enumerate(wd[::2]):
+                    if ol==[t.shape,wd[(it*2)+1].shape]:
+                        print("importing weights:wshape={},bias shape={}".format(t.shape,wd[it+1].shape))
+                        model.layers[ix+1].set_weights((t,wd[(it*2)+1]))
+                        if frezimp:
+                            model.layers[ix+1].trainable=False
+                        break
     model.compile(loss=k.losses.categorical_crossentropy,
                   optimizer=optim
                   ,metrics=['accuracy'])
+
     k.utils.print_summary(model)
+    #print(tftrain[0].shape)
     sess=tf.Session()
     with sess.as_default():
         sess.run(tf.global_variables_initializer())
         hardlr=(copy.deepcopy(sess.run(optim.lr))*2)
-    def step_decay(epoch):
+    def step_decay(epoch):#extra for the callback
         drop = 0.5
         epochs_drop = 10.0
         lrate = hardlr * drop** floor((1+epoch)/epochs_drop)
@@ -180,28 +205,41 @@ def kertrain(*args):
               k.callbacks.LearningRateScheduler(step_decay)]
     fitres=model.fit(x=tftrain[0],y=tftrain[1],batch_size=batchs,epochs=epochs,
               validation_split=0.05,validation_data=(tftest[0],tftest[1]),
-              shuffle=False,verbose=2,callbacks=callbako)
-    if eval:
+              shuffle=False,callbacks=callbako)#,verbose=2,)
+    if evalu:
         evres=model.evaluate(tftest[0], y=tftest[1], batch_size=batchs, verbose=0,callbacks=callbako)
         return(fitres,evres)
     return(fitres)
 
+
+if importW:
+    md=k.applications.VGG16()#input_shape=tftrain[0].shape,classes=outshp)
+    wl=md.get_weights()
+    del md
+    wl=wl[:8]+wl[-4:]
+    unich=[wl[0][:,:,0:1,:],wl[1]]
+    wl=unich+wl
+else:
+    wl=None
+    
+
 if K:
     returned=dict()
-    todo=[kerdict,]
-    #hsto=kertrain(*kerdict)
-    #print(hsto.params)#good
-    #print(hsto.history)#1 value per epoch per metric
+    if todo is None:
+        todo=[kerdict,]
     print(kerdict)
     for parms in todo:
-        if eval:
-            histob,histob2=kertrain(*parms)
-            returned[histob.params]=histob.history
-            returned[histob2.params]=histob2.history
+        print(todo)
+        if evalu:
+            histob,histob2=kertrain(*parms,wd=wl,frezimp=frezimp)
+            returned[str(histob.params)]=histob.history
+            returned[str(histob2.params)]=histob2.history
         else:
-            histob=kertrain(*parms)
-            returned[histob.params]=histob.history
+            histob=kertrain(*parms,wd=wl,frezimp=frezimp)
+            returned[str(histob.params)]=histob.history
     print(returned)
-    tdt=dt.date.today()
-    with open("protolog_{}{}-{}.txt".format(tdt.year,tdt.timetuple()[-2],ti.ctime().split(" ")[-2].replace(":","-"))) as fl:
-        fl.write(returned)
+    if filelog:
+        tdt=dt.date.today()
+        with open("protolog_{}{}-{}.txt".format(tdt.year,tdt.timetuple()[-2],ti.ctime().split(" ")[-2].replace(":","-"))) as fl:
+            fl.write(returned)
+    
